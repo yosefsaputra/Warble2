21f84706bb2c374aaf86d163e8d8084b430d25d9
yosefsaputra <yosef.saputra@gmail.com>
2017-11-29 18:32:31 -0600

    To replace old main activity with new one

--- app/src/main/java/mpc/utexas/edu/warble2/ui/MainActivity.java
+++ app/src/main/java/mpc/utexas/edu/warble2/ui/MainActivity.java
@@ -1,286 +1,133 @@
 package mpc.utexas.edu.warble2.ui;
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
-
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.net.MulticastSocket;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import mpc.utexas.edu.warble2.R;
-import mpc.utexas.edu.warble2.things.Bridge;
-import mpc.utexas.edu.warble2.things.PhilipsHue.PhilipsBridge;
+import mpc.utexas.edu.warble2.ui.fragments.InfoFragment;
+import mpc.utexas.edu.warble2.ui.fragments.SettingsFragment;
+import mpc.utexas.edu.warble2.ui.fragments.SwitchFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
     public static String username = "yosef";
     public static String userid = "QVhaSMPsPn-VVA5KwxSks3Lj1LAp92Wz3SWzWYN3";
-
-//    PhilipsHueService mService = PhilipsHueUtil.getService("http://192.168.1.74");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-        startActivity(intent);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
     }
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        setUsernameView();
-//        setUseridView();
-//        setCreateUserButton();
-//        setShowInfoButton();
-//
-//        setLight1OffButton();
-//        setLight1OnButton();
-//
-//        new SSDPDiscovery().execute();
-//    }
-//
-//    public class SSDPDiscovery extends AsyncTask<Void, Void, List<Bridge>> {
-//        @Override
-//        protected List<Bridge> doInBackground(Void... params){
-//            List<String> upnp_messages = new ArrayList<>();
-//            try {
-//                String DISCOVER_MESSAGE = "M-SEARCH * HTTP/1.1\r\n"
-//                        + "HOST: 239.255.255.250:1900\r\n"
-//                        + "MAN: \"ssdp:discover\"\r\n"
-//                        + "MX: 3\r\n"
-//                        + "ST: ssdp:all\r\n";
-//                MulticastSocket socket = new MulticastSocket(1900);
-//                InetAddress host = InetAddress.getByName("239.255.255.250");
-//                DatagramPacket sendPacket = new DatagramPacket(DISCOVER_MESSAGE.getBytes(), DISCOVER_MESSAGE.length(), host, 1900);
-//                socket.setSoTimeout(5000);
-//                socket.send(sendPacket);
-//
-//                long startTime = System.currentTimeMillis();
-//                while (System.currentTimeMillis() - startTime < 5000) {
-//                    byte[] buf = new byte[1024];
-//                    DatagramPacket receivePacket = new DatagramPacket(buf, buf.length);
-//                    socket.receive(receivePacket);
-//
-//                    String msg = new String(receivePacket.getData(), 0, receivePacket.getLength());
-//                    upnp_messages.add(msg);
-//
-//                    // If sleep is not given, Philips HUE bridge is not found consistently. For some reasons.
-//                    try {
-//                        TimeUnit.MILLISECONDS.sleep(100);
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//
-//            List<Bridge> bridges = new ArrayList<>();
-//            List<String> uuid_bridges = new ArrayList<>();
-//            for (String upnp_message : upnp_messages) {
-//                System.out.println(upnp_message);
-//                if (upnp_message.contains("IpBridge")) {
-//                    Pattern pattern = Pattern.compile("LOCATION: (.+?)(\\r*)(\\n)", Pattern.DOTALL | Pattern.MULTILINE);
-//                    Matcher matcher = pattern.matcher(upnp_message);
-//                    if (matcher.find()) {
-//                        String xml_url = matcher.group(1);
-//                        Document document = requestWebpage(xml_url);
-//                        Bridge bridge = createBridgeFromXmlDocument(document);
-//                        if (!uuid_bridges.contains(bridge.getUUID())) {
-//                            bridges.add(bridge);
-//                            uuid_bridges.add(bridge.getUUID());
-//                        }
-//                    }
-//                }
-//            }
-//
-//            return bridges;
-//        }
-//
-//        @Override
-//        protected void onPostExecute(List<Bridge> bridges) {
-//            ProgressBar asyncTaskProgressBar = findViewById(R.id.progressBar);
-//            asyncTaskProgressBar.setVisibility(View.INVISIBLE);
-//
-//            List<String> bridge_names = new ArrayList<>();
-//            for (Bridge bridge: bridges) {
-//                bridge_names.add(bridge.getUUID());
-//            }
-//
-//            ListView bridgeListView = findViewById(R.id.listBridgesView);
-//            ArrayAdapter<Bridge> adapter = new BridgeArrayAdapter(getApplicationContext(), bridges);
-//            bridgeListView.setAdapter(adapter);
-//        }
-//
-//        private Document requestWebpage(String string_url) {
-//            Document document = null;
-//            try {
-//                URL url = new URL(string_url);
-//
-//                SAXBuilder saxBuilder = new SAXBuilder();
-//                document = saxBuilder.build(url);
-//            } catch (IOException | JDOMException e) {
-//                e.printStackTrace();
-//            }
-//            return document;
-//        }
-//
-//        private Bridge createBridgeFromXmlDocument(Document document) {
-//            Element rootElement = document.getRootElement();
-//            Element URLBase = rootElement.getChild("URLBase", rootElement.getNamespace());
-//            Element device = rootElement.getChild("device", rootElement.getNamespace());
-//
-//            String name = device.getChild("friendlyName", rootElement.getNamespace()).getText();
-//            String modelName = device.getChild("modelName", rootElement.getNamespace()).getText();;
-//            String id = device.getChild("UDN", rootElement.getNamespace()).getText().replace("uuid:", "");
-//            String urlBase = URLBase.getText();
-//
-//            if (modelName.contains("Philips")) {
-//                PhilipsBridge bridge = new PhilipsBridge(name, id, urlBase);
-//                bridge.setUUID(id);
-//                return bridge;
-//            } else {
-//                Bridge bridge = new Bridge(name, id, urlBase);
-//                bridge.setUUID(id);
-//                return bridge;
-//            }
-//        }
-//    }
-//
-//    public class BridgeArrayAdapter extends ArrayAdapter<Bridge> {
-//        private final Context context;
-//        private final List<Bridge> bridges;
-//
-//        public BridgeArrayAdapter(Context context, List<Bridge> bridges) {
-//            super(context, -1, bridges);
-//            this.context = context;
-//            this.bridges = bridges;
-//        }
-//
-//        @Override
-//        public View getView(int position, View convertView, ViewGroup parent) {
-//            LayoutInflater inflater = (LayoutInflater) context
-//                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//            View rowView = inflater.inflate(R.layout.bridge_layout, parent, false);
-//            TextView textView = (TextView) rowView.findViewById(R.id.textView);
-//            textView.setText(bridges.get(position).getName());
-//            return rowView;
-//        }
-//    }
-//
-//
-//    private void setUsernameView() {
-//        TextView usernameView = findViewById(R.id.username);
-//        if (username == null) {
-//            usernameView.setText("null");
-//        } else {
-//            usernameView.setText(username);
-//        }
-//    }
-//
-//    private void setUseridView() {
-//        TextView useridView = findViewById(R.id.userid);
-//
-//        if (userid == null) {
-//            useridView.setText("null");
-//        } else {
-//            useridView.setText(userid);
-//        }
-//    }
-//
-//    private void setCreateUserButton() {
-//        Button createUserButton = findViewById(R.id.createUserButton);
-//        createUserButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent createUser = new Intent(MainActivity.this, CreateUserActivity.class);
-//                createUser.putExtra("username", username);
-//                createUser.putExtra("userid", userid);
-//                startActivity(createUser);
-//            }
-//        });
-//    }
-//
-//    private void setShowInfoButton() {
-//        Button showInfoButton = findViewById(R.id.showInfoButton);
-//        showInfoButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent createUser = new Intent(MainActivity.this, ShowInfoActivity.class);
-//                createUser.putExtra("username", username);
-//                createUser.putExtra("userid", userid);
-//                startActivity(createUser);
-//            }
-//        });
-//    }
-//
-//    private HashMap<String, Object> returnLightStateHashMap(boolean on, int bri) {
-//        HashMap<String, Object> lightState = new HashMap<>();
-//        lightState.put("on", on);
-//        lightState.put("bri", bri);
-//        lightState.put("transitiontime", 0);
-//
-//        return lightState;
-//    }
 
-//    private void setLight1OnButton() {
-//        final Button light1OnButton = findViewById(R.id.light1OnButton);
-//        light1OnButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//            mService.putLight(userid, "10", returnLightStateHashMap(true, 50)).enqueue(new Callback<List<Object>>() {
-//                @Override
-//                public void onResponse(Call<List<Object>> call, Response<List<Object>> response) {
-//                    PhilipsUser user = new PhilipsUser(username, userid);
-//                    PhilipsLight light = new PhilipsLight("10", "10", user);
-//                    light.setOn();
-//                }
-//
-//                @Override
-//                public void onFailure(Call<List<Object>> call, Throwable t) {}
-//            });
-//            }
-//        });
-//    }
-//
-//    private void setLight1OffButton() {
-//        Button light1OffButton = findViewById(R.id.light1OffButton);
-//        light1OffButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                PhilipsUser user = new PhilipsUser(username, userid);
-//                PhilipsLight light = new PhilipsLight("10", "10", user);
-//                light.setOff();
-//            }
-//        });
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            switch (position) {
+                case 0:
+                    SwitchFragment switchFragment = new SwitchFragment();
+                    return switchFragment;
+                case 1:
+                    InfoFragment infoFragment = new InfoFragment();
+                    return infoFragment;
+                case 2:
+                    SettingsFragment settingsFragment = new SettingsFragment();
+                    return settingsFragment;
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Switch";
+                case 1:
+                    return "Info";
+                case 2:
+                    return "Settings";
+                default:
+                    return null;
+            }
+        }
+    }
 }
